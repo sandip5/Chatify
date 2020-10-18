2e1ecae7d2c48ca528fd7216e86f2ccfd35f80d0
sandeep <singhsandeep.singh97@gmail.com>
2020-10-18 15:49:31 +0530

    REFACTORED- Indentation And Removed Extra Line

--- chat/client/model/client.h
+++ chat/client/model/client.h
@@ -22,7 +22,7 @@ int my_sock;
 int client_sock;
 int their_addr_size;
 int portno;
-pthread_t send_t,recv_t, login_t;
+pthread_t send_t, recv_t, login_t;
 char msg[500];
 char username[100];
 char res[600];
@@ -41,79 +41,78 @@ int connect_client_to_server()
 {
 	portno = 8888;
 	my_sock = socket(AF_INET, SOCK_STREAM, 0);
-	memset(their_addr.sin_zero,'\0',sizeof(their_addr.sin_zero));
+	memset(their_addr.sin_zero, '\0', sizeof(their_addr.sin_zero));
 	their_addr.sin_family = AF_INET;
 	their_addr.sin_port = htons(portno);
 	their_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-	if(connect(my_sock,(struct sockaddr *)&their_addr,sizeof(their_addr)) < 0) 
-    {
+	if (connect(my_sock, (struct sockaddr *)&their_addr, sizeof(their_addr)) < 0)
+	{
 		perror("connection not established");
 		exit(1);
 	}
 
 	inet_ntop(AF_INET, (struct sockaddr *)&their_addr, ip, INET_ADDRSTRLEN);
-	printf("connected to %s",ip);
+	printf("connected to %s", ip);
 	server_response_handler();
-    
-    return 0;
-}
 
+	return 0;
+}
 
 void server_response_handler()
 {
 	bool flag = true;
 
-	while(flag)
+	while (flag)
 	{
-		memset(msg,'\0',sizeof(msg));
+		memset(msg, '\0', sizeof(msg));
 		recv(my_sock, msg, 500, 0);
 
-        switch (msg[0])
-        {
-            case logged_in_successfully:
-		        {
-			        std::cout << "\nLogged In Successfully..." << std::endl;
-			        start_chat();
-                    flag = false;
-		        }
-                break;
-		    case invalid_id_password:
-		        {
-			        std::cout << "\nInvalid UserId or Password" << std::endl;
-		        }
-                break;
-		    case exit_client:
-		        {
-			        std::cout << "\nThank You!!!" << std::endl;
-                    flag = false;
-		        }
-                break;
-		    case already_registered:
-		        {
-			        std::cout << "\nUser Is Already Registered. Try With Different User Id..." << std::endl;
-		        }
-                break;
-		    case already_logged_in:
-		        {
-			        std::cout << "\nUser Is Already Logged In. Try Again..." << std::endl;
-		        }   
-                break;
-		    case registered_successfully:
-		        {
-			        std::cout << "\nRegistered Successfully And Logged In..." << std::endl;
-			        start_chat();
-			        flag = false;
-		        }
-                break;
-		    case already_registered_logged_in:
-		        {
-			        std::cout << "\nUser Is Already Registered And Logged In. Try Again..." << std::endl;
-		        }
-        }
-		
+		switch (msg[0])
+		{
+		case logged_in_successfully:
+		{
+			std::cout << "\nLogged In Successfully..." << std::endl;
+			start_chat();
+			flag = false;
+		}
+		break;
+		case invalid_id_password:
+		{
+			std::cout << "\nInvalid UserId or Password" << std::endl;
+		}
+		break;
+		case exit_client:
+		{
+			std::cout << "\nThank You!!!" << std::endl;
+			flag = false;
+		}
+		break;
+		case already_registered:
+		{
+			std::cout << "\nUser Is Already Registered. Try With Different User Id..." << std::endl;
+		}
+		break;
+		case already_logged_in:
+		{
+			std::cout << "\nUser Is Already Logged In. Try Again..." << std::endl;
+		}
+		break;
+		case registered_successfully:
+		{
+			std::cout << "\nRegistered Successfully And Logged In..." << std::endl;
+			start_chat();
+			flag = false;
+		}
+		break;
+		case already_registered_logged_in:
+		{
+			std::cout << "\nUser Is Already Registered And Logged In. Try Again..." << std::endl;
+		}
+		}
+
 		std::cout << msg;
-		memset(msg,'\0',sizeof(msg));
+		memset(msg, '\0', sizeof(msg));
 		fgets(msg, 500, stdin);
 		write(my_sock, msg, strlen(msg));
 	}
@@ -122,21 +121,21 @@ void server_response_handler()
 void start_chat()
 {
 	pthread_create(&recv_t, NULL, recv_msg, &my_sock);
-	while(fgets(msg,500,stdin) > 0) 
-    {
+	while (fgets(msg, 500, stdin) > 0)
+	{
 		strcpy(res, username);
-		strcat(res,">>");
-		strcat(res,msg);
-		len = write(my_sock,res,strlen(res));
-		if(len < 0) 
+		strcat(res, ">>");
+		strcat(res, msg);
+		len = write(my_sock, res, strlen(res));
+		if (len < 0)
 		{
 			perror("message not sent");
 			exit(1);
 		}
-		memset(msg,'\0',sizeof(msg));
-		memset(res,'\0',sizeof(res));
+		memset(msg, '\0', sizeof(msg));
+		memset(res, '\0', sizeof(res));
 	}
-	pthread_join(recv_t,NULL);
+	pthread_join(recv_t, NULL);
 	close(my_sock);
 }
 
@@ -145,12 +144,10 @@ void *recv_msg(void *sock)
 	int client_sock = *((int *)sock);
 	char msg[500];
 	int len;
-	while((len = recv(client_sock,msg,500,0)) > 0) {
+	while ((len = recv(client_sock, msg, 500, 0)) > 0)
+	{
 		msg[len] = '\0';
-		fputs(msg,stdout);
-		memset(msg,'\0',sizeof(msg));
+		fputs(msg, stdout);
+		memset(msg, '\0', sizeof(msg));
 	}
 }
-
-
-
